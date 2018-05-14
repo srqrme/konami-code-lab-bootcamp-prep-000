21c84ad473a33701bf554f67b37f63837bbab105
Sharmaine Requerme <srqrme@gmail.com>
2018-05-14 17:42:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,20 +13,22 @@ const codes = [
 
 function init() {
   // your code here
-  let index = 0;
+  let index = 0
 
-  document.body.addEventListener("keydown", function (e))
-    const key = e.key;
+  function onKeyDownHandler(e) {
+    document.body.addEventListener("keydown", function (e)) {
+      const key = e.key
 
-    if (key === codes[index]) {
-      index++;
+      if (key === codes[index]) {
+        index++
 
-      if (index === codes.length) {
-        window.alert("Hurray!");
+        if (index === codes.length)
+        window.alert("Hurray!")
 
-        index = 0;
+        index = 0
+      }
+    } else {
+      index = 0
     }
-  } else {
-    index = 0;
   }
 }
